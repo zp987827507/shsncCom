0a0a48bb955859836219bf3ac34cd90390348bb7
zp987827507 <15818573206@163.com>
2022-05-23 18:34:33 +0800

    deploy

--- docs/.vuepress/components/table-md.vue
+++ docs/.vuepress/components/table-md.vue
@@ -1,32 +1,98 @@
 <template>
-  <div>
-    <table class="zTable">
-      <tr>
-        <td>1</td>
-        <td>2</td>
-        <td>3</td>
-      </tr>
-      <tr>
-        <td>1</td>
-        <td>2</td>
-        <td>3</td>
-      </tr>
-    </table>
-  </div>
+    <div class="zTableCom zTableComMd">
+        <el-table
+            :data="data"
+            :max-height="maxHeight"
+            :border="isShowborder"
+            @selection-change="handleSelectionChange"
+            style="width: 100%"
+        >
+            <el-table-column v-if="checkbox" type="selection" width="50"></el-table-column>
+            <el-table-column v-if="index" type="index" width="50"> </el-table-column>
+            <el-table-column
+                v-for="item in column"
+                :key="item.prop"
+                :prop="item.prop"
+                :label="item.label"
+                :width="item.width"
+                :sortable="item.sort"
+            >
+                <template slot-scope="scope">
+                    <slot v-if="item.type === 'slot'" :name="item.slot_name" :data="scope.row"></slot>
+                    <component
+                        v-else
+                        :data="scope.row"
+                        :config="item"
+                        :is="!item.type ? 'com-text' : `com-${item.type}`"
+                    ></component>
+                </template>
+            </el-table-column>
+        </el-table>
+    </div>
 </template>
 
 <script>
-export default {
-  name: "zTable",
-};
+    import '../../../packages/src/css/style.scss'
+    //自动化规则，通过type属性自动获取目录组件
+    //true会去读取子目录下得文件  false不会读取
+    //第3个参数  /index.vue$/ 表示指定读取index.vue得文件
+    let modules = {} //引入得组件
+    const files = require.context('../../../packages/components/table/control', true, /index.vue$/)
+    files.keys().forEach((item) => {
+        const key = item.split('/')
+        const name = key[1]
+        const component = files(item).default
+        modules[`com-${name}`] = component
+    })
+    export default {
+        name: 'zTable',
+        props: {
+            checkList: {
+                type: Array,
+                default: () => [],
+            },
+            column: {
+                //显示每一列得字段属性
+                type: Array,
+                default: () => [],
+            },
+            data: {
+                //table数据
+                type: Array,
+                default: () => [],
+            },
+            checkbox: Boolean, //是否显示复选框
+            index: Boolean, //是否显示排序
+            isShowborder: Boolean, //是否显示边框
+            maxHeight: {
+                //表格最大高度  number类型
+                type: Number,
+                //default: () => 200,
+            },
+        },
+        components: {
+            ...modules,
+        },
+        mounted() {},
+        data() {
+            return {}
+        },
+        methods: {
+            handleSelectionChange(row) {
+                this.$emit('update:checkList', row)
+            },
+        },
+    }
 </script>
 
-<style lang="scss" scoped>
-.zTable{
-  tr{
-    td{
-      color:red
+<style lang="scss">
+    .zTableComMd {
+        .el-table__body,
+        .el-table__header {
+            margin: 0;
+        }
+        .el-table__cell {
+            border: 0;
+        }
     }
-  }
-}
 </style>
--- docs/componentsDocs/table.md
+++ docs/componentsDocs/table.md
@@ -1,11 +1,74 @@
 # Table
 
-表格组件
+### 公共代码
 
 ### 示例
 
-<table-md />
-<img src="/favicon.ico"/>
+<br/>
+<table-md :column="column" checkbox :check-list.sync="check_list" :data="tableData">
+        <!-- v-slot:operation="slotData" 指定插槽和获取插槽传入得数据 -->
+        <template v-slot:operation="slotData">
+            <!-- 获取slot传入得数据 -->
+            <el-button size="mini" type="primary" @click="edit(slotData.data)">编辑</el-button>
+            <el-button size="mini">删除</el-button>
+        </template>
+    </table-md>
+<br/>
+
+<script>
+    export default {
+        data() {
+            return {
+                column: [
+                    {
+                        type: 'function',
+                        label: 'URL地址',
+                        prop: 'url',
+                        width: 200,
+                        callback: (row) => {
+                            //在callback里面自定义格式
+                            //如果type是function  则返回每一行得数据
+                            //console.log(row)
+                            return `<a href="http://www.baidu.com">${row.name}</a>`
+                        },
+                    },
+                    {
+                        label: '日期',
+                        prop: 'date',
+                        width: 200,
+                        sort: true, //是否排序
+                    },
+                    {
+                        type: 'slot',
+                        prop: 'operation',
+                        slot_name: 'operation',
+                        width:150,
+                        label: '操作',
+                    },
+                ],
+                check_list: [], //复选框勾选得数据
+                tableData: [
+                    {
+                        date: '2016-05-02',
+                        name: '王小虎',
+                        address: '上海市普陀区金沙江路 1518 弄',
+                        sex: '男',
+                    },
+                    {
+                        date: '2016-05-04',
+                        name: '王小虎',
+                        address: '上海市普陀区金沙江路 1517 弄',
+                    },
+                    {
+                        date: '2016-05-01',
+                        name: '王小虎',
+                        address: '上海市普陀区金沙江路 1519 弄',
+                    },
+                ],
+            }
+        },
+    }
+</script>
 
 ### 代码
 
