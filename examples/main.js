1fb37f966951cafccb10853219cc3a83016dd97a
tanchaoju <1120153473@qq.com>
2022-05-23 10:59:06 +0800

    element-ui改为项目引入，不再在组件内单独引入

--- examples/main.js
+++ examples/main.js
@@ -2,10 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import shsncCom from '../packages'
-// import elem from 'element-ui'
-// import 'element-ui/lib/theme-chalk/index.css'
+import elem from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 Vue.config.productionTip = false
-// Vue.use(elem)
+Vue.use(elem)
 
 Vue.use(shsncCom)
 new Vue({
